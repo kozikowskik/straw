51bce3c940d9a4e031ee4142e804c5b28b25f3c4
Karol Kozikowski <kozikowskik@gmail.com>
2026-06-05 14:32:31 +0000

    test: add public API examples

--- example_test.go
+++ example_test.go
@@ -1,19 +1,123 @@
-package straw
+package straw_test
 
 import (
 	"fmt"
 
 	tea "charm.land/bubbletea/v2"
+	"github.com/kozikowskik/straw"
 )
+
+func ExampleKey() {
+	var key straw.Key = straw.Text("g")
+	sequence := straw.Sequence(key)
+
+	fmt.Println(len(sequence))
+
+	// Output:
+	// 1
+}
+
+func ExampleSeq() {
+	var sequence straw.Seq = straw.TextSequence("gh")
+
+	fmt.Println(len(sequence))
+
+	// Output:
+	// 2
+}
+
+func ExampleText() {
+	sequence := straw.Sequence(straw.Text("g"))
+
+	fmt.Println(len(sequence))
+
+	// Output:
+	// 1
+}
+
+func ExampleTextSequence() {
+	sequence := straw.TextSequence("gé")
+
+	fmt.Println(len(sequence))
+
+	// Output:
+	// 2
+}
+
+func ExampleCode() {
+	sequence := straw.Sequence(straw.Code(tea.KeyEsc))
+
+	fmt.Println(len(sequence))
+
+	// Output:
+	// 1
+}
+
+func ExampleModified() {
+	sequence := straw.Sequence(straw.Modified('c', tea.ModCtrl))
+
+	fmt.Println(len(sequence))
+
+	// Output:
+	// 1
+}
+
+func ExampleSequence() {
+	sequence := straw.Sequence(
+		straw.Text("g"),
+		straw.Code(tea.KeyEsc),
+		straw.Modified('c', tea.ModCtrl),
+	)
+
+	fmt.Println(len(sequence))
+
+	// Output:
+	// 3
+}
+
+func ExampleBinding() {
+	type action int
+
+	const goHome action = 1
+
+	var binding straw.Binding[action] = straw.Bind(goHome, straw.TextSequence("gh"))
+
+	fmt.Println(binding.Action())
+
+	// Output:
+	// 1
+}
+
+func ExampleBindingOption() {
+	var option straw.BindingOption = straw.Description("go home")
+	binding := straw.Bind("go-home", straw.TextSequence("gh"), option)
+
+	fmt.Println(binding.Description())
+
+	// Output:
+	// go home
+}
+
+func ExampleDescription() {
+	binding := straw.Bind("go-home",
+		straw.TextSequence("gh"),
+		straw.Description("go home"),
+	)
+
+	fmt.Println(binding.Description())
+
+	// Output:
+	// go home
+}
 
 func ExampleBind() {
 	type action int
 
 	const goHome action = 1
 
-	binding := Bind(goHome,
-		TextSequence("gh"),
-		Description("go home"),
+	binding := straw.Bind(goHome,
+		straw.TextSequence("gh"),
+		straw.Description("go home"),
 	)
 
 	fmt.Println(binding.Action())
@@ -26,15 +130,32 @@ func ExampleBind() {
 	// 2
 }
 
-func ExampleSequence() {
-	sequence := Sequence(
-		Text("g"),
-		Code(tea.KeyEsc),
-		Modified('c', tea.ModCtrl),
-	)
+func ExampleBinding_Action() {
+	binding := straw.Bind("go-home", straw.TextSequence("gh"))
 
-	fmt.Println(len(sequence))
+	fmt.Println(binding.Action())
 
 	// Output:
-	// 3
+	// go-home
+}
+
+func ExampleBinding_Sequence() {
+	binding := straw.Bind("go-home", straw.TextSequence("gh"))
+
+	fmt.Println(len(binding.Sequence()))
+
+	// Output:
+	// 2
+}
+
+func ExampleBinding_Description() {
+	binding := straw.Bind("go-home",
+		straw.TextSequence("gh"),
+		straw.Description("go home"),
+	)
+
+	fmt.Println(binding.Description())
+
+	// Output:
+	// go home
 }
